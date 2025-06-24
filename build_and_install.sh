16bc108230acd186f20082e0d7ea591e6ddc8ba6
«NikitaMarakushev» <nikitamarakushev@yandex.ru>
2025-06-24 21:29:52 +0300

    added bash func colored echo

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+function color_echo() {
+    local color=$1
+    shift
+    echo -e "\e[${color}m$*\e[0m"
+}
 
-echo "################RUN BUILD##############\n"
+echo -e "\n"
+color_echo "34" "################ RUN BUILD ##############"
+
+echo -e "\n"
+
 python -m build
-echo "################BULD SUCCEED###########\n"
+color_echo "34" "################BULD SUCCEED###########\n"
 
-echo "################INSTALL OBSIDIAN BACKUPER##############\n"
+color_echo "34" "################INSTALL OBSIDIAN BACKUPER##############\n"
 pip install dist/obsidian_backuper-1.0.2-py3-none-any.whl
-echo "################OBSIDIAN BACKUPER INSTALLED#############\n"
+color_echo "34" "################OBSIDIAN BACKUPER INSTALLED#############\n"
