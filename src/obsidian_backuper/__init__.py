5751eef5abdbd5d30b3e7c2362494b29148b3482
«NikitaMarakushev» <nikitamarakushev@yandex.ru>
2025-06-29 21:04:37 +0300

    1.0.0 release

--- src/obsidian_backuper/__init__.py
+++ src/obsidian_backuper/__init__.py
@@ -2,5 +2,5 @@ from .core import ObsidianBackuper
 from .cli import main
 from .exceptions import ObsidianBackupError
 
-__version__ = "1.0.4"
+__version__ = "1.0.0"
 __all__ = ["ObsidianBackuper", "ObsidianDecryptor", "main", "ObsidianBackupError", "DecryptionError"]
